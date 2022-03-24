957ef3e14e901b475ee885c959486c4007a8f9d5
Y.Paderin <yuriypaderin@gmail.com>
2022-03-24 22:13:40 +0300

    Changed demo project.

--- QueryBuilderSandbox/Program.cs
+++ QueryBuilderSandbox/Program.cs
@@ -12,31 +12,32 @@ namespace QueryBuilderSandbox
 		{
 			ISource table1 = new Table("table1", "t1");
 			ISource table2 = new Table("table2", null, "public");
-			ISource table3 = new Table("table3");
+			ISource table3 = new Table("table3", "t3");
 
 			IColumn column1 = new Column("column1", "c1", "table1");
 			IColumn column2 = new Column("column2", null, new Table("table1", "t1"));
 			IColumn column3 = new Column("column3", null, new Table("table2", null, "public"));
 			IColumn column4 = new Column("column4", "c4", table3);
 
+			new InCondition(column1, new IValue[] { (Int32Value)32, (StringValue)"test" });
+
 			Select select = new Select(column1, column2, column3, column4)
-				.From(table1, table2, table3)
-				.Where(
-					new AndCondition(
-						new ICondition[] {
-							new EqualCondition(column1, column2),
-							new LikeCondition(column2, "%test%"),
-							new BetweenCondition(column1, column2, new StringValue("Test")),
-							new OrCondition(
-								new ICondition[] {
-									new NotEqualCondition(column3, column4),
-									new IsNullCondition(column3)
-								})
-						}))
+				.From(table1, table2)
+				.LeftJoin(table3, cb => cb.Equal(column3, column4))
+				.Where(cb =>
+				{
+					cb.Equal(column1, "test");
+					cb.Greater(column2, 33);
+					cb.And(cb =>
+					{
+						cb.IsNotNull(column3);
+						cb.Like(column3, "%name%");
+					});
+				})
 				.GroupBy("table1")
 				.OrderByAsc("table1")
 				.Offset(100)
-				.Limit(100);
+				.Limit(10);
 
 			IRenderer renderer = new PostgreSqlRenderer();
 
