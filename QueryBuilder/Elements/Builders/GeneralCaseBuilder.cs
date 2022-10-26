c9e2adbd42ce6ec3412f815322eb4e5d48ef28c4
Y.Paderin <yuriypaderin@gmail.com>
2022-10-26 20:33:04 +0300

    Add methods overrides for switch case builders.

--- QueryBuilder/Elements/Builders/GeneralCaseBuilder.cs
+++ QueryBuilder/Elements/Builders/GeneralCaseBuilder.cs
@@ -11,6 +11,22 @@ namespace YuraSoft.QueryBuilder
 		private List<Tuple<ICondition, IExpression>> _whenThens = new List<Tuple<ICondition, IExpression>>();
 		private IExpression? _else;
 
+		public GeneralCaseBuilder WhenThen(Action<ConditionBuilder> conditionBuildMethod, string column) => WhenThen(conditionBuildMethod, new SourceColumn(column));
+		public GeneralCaseBuilder WhenThen(Action<ConditionBuilder> conditionBuildMethod, string column, string table) => WhenThen(conditionBuildMethod, new SourceColumn(column, new Table(table)));
+		public GeneralCaseBuilder WhenThen(Action<ConditionBuilder> conditionBuildMethod, string column, ISource source) => WhenThen(conditionBuildMethod, new SourceColumn(column, source));
+
+		public GeneralCaseBuilder WhenThen(Action<ConditionBuilder> conditionBuildMethod, IExpression expression)
+		{
+			ConditionBuilder conditionBuilder = new ConditionBuilder();
+			conditionBuildMethod.Invoke(conditionBuilder);
+
+			return WhenThen(conditionBuilder.Build(), expression);
+		}
+
+		public GeneralCaseBuilder WhenThen(ICondition condition, string column) => WhenThen(condition, new SourceColumn(column));
+		public GeneralCaseBuilder WhenThen(ICondition condition, string column, string table) => WhenThen(condition, new SourceColumn(column, new Table(table)));
+		public GeneralCaseBuilder WhenThen(ICondition condition, string column, ISource source) => WhenThen(condition, new SourceColumn(column, source));
+
 		public GeneralCaseBuilder WhenThen(ICondition condition, IExpression expression)
 		{
 			_whenThens.Add(Tuple.Create(condition, expression));
@@ -18,6 +34,9 @@ namespace YuraSoft.QueryBuilder
 			return this;
 		}
 
+		public void Else(string column) => _else = new SourceColumn(column);
+		public void Else(string column, string table) => _else = new SourceColumn(column, new Table(table));
+		public void Else(string column, ISource source) => _else = new SourceColumn(column, source);
 		public void Else(IExpression expression) => _else = expression;
 
 		public GeneralCaseExpression Build()
--- QueryBuilder/Elements/Builders/SimpleCaseBuilder.cs
+++ QueryBuilder/Elements/Builders/SimpleCaseBuilder.cs
@@ -12,10 +12,42 @@ namespace YuraSoft.QueryBuilder
 		private List<Tuple<IExpression, IExpression>> _whenThens = new List<Tuple<IExpression, IExpression>>();
 		private IExpression? _else;
 
+		public SimpleCaseBuilder(string column)
+		{
+			Validator.ThrowIfArgumentIsNullOrEmpty(column, nameof(column));
+
+			_expression = new SourceColumn(column);
+		}
+
+		public SimpleCaseBuilder(string column, string table)
+		{
+			Validator.ThrowIfArgumentIsNullOrEmpty(column, nameof(column));
+			Validator.ThrowIfArgumentIsNullOrEmpty(table, nameof(table));
+
+			_expression = new SourceColumn(column, new Table(table));
+		}
+
+		public SimpleCaseBuilder(string column, ISource source)
+		{
+			Validator.ThrowIfArgumentIsNullOrEmpty(column, nameof(column));
+			Validator.ThrowIfArgumentIsNull(source, nameof(source));
+
+			_expression = new SourceColumn(column, source);
+		}
+
 		public SimpleCaseBuilder(IExpression expression)
 		{
 			_expression = Validator.ThrowIfArgumentIsNull(expression, nameof(expression));
 		}
+
+		public SimpleCaseBuilder WhenThen(string firstColumn, string secondColumn) => WhenThen(new SourceColumn(firstColumn), new SourceColumn(secondColumn));
+		public SimpleCaseBuilder WhenThen(string firstColumn, string firstTable, string secondColumn) => WhenThen(new SourceColumn(firstColumn, new Table(firstTable)), new SourceColumn(secondColumn));
+		public SimpleCaseBuilder WhenThen(string firstColumn, ISource firstSource, string secondColumn) => WhenThen(new SourceColumn(firstColumn, firstSource), new SourceColumn(secondColumn));
+		public SimpleCaseBuilder WhenThen(string firstColumn, string secondColumn, ISource secondSource) => WhenThen(new SourceColumn(firstColumn), new SourceColumn(secondColumn, secondSource));
+		public SimpleCaseBuilder WhenThen(string firstColumn, string firstTable, string secondColumn, string secondTable) => WhenThen(new SourceColumn(firstColumn, new Table(firstTable)), new SourceColumn(secondColumn, new Table(secondTable)));
+		public SimpleCaseBuilder WhenThen(string firstColumn, ISource firstSource, string secondColumn, string secondTable) => WhenThen(new SourceColumn(firstColumn, firstSource), new SourceColumn(secondColumn, new Table(secondTable)));
+		public SimpleCaseBuilder WhenThen(string firstColumn, string firstTable, string secondColumn, ISource secondSource) => WhenThen(new SourceColumn(firstColumn, new Table(firstTable)), new SourceColumn(secondColumn, secondSource));
+		public SimpleCaseBuilder WhenThen(string firstColumn, ISource firstSource, string secondColumn, ISource secondSource) => WhenThen(new SourceColumn(firstColumn, firstSource), new SourceColumn(secondColumn, secondSource));
 
 		public SimpleCaseBuilder WhenThen(IExpression condition, IExpression expression)
 		{
@@ -24,6 +56,9 @@ namespace YuraSoft.QueryBuilder
 			return this;
 		}
 
+		public void Else(string column) => _else = new SourceColumn(column);
+		public void Else(string column, string table) => _else = new SourceColumn(column, new Table(table));
+		public void Else(string column, ISource source) => _else = new SourceColumn(column, source);
 		public void Else(IExpression expression) => _else = expression;
 
 		public SimpleCaseExpression Build()
