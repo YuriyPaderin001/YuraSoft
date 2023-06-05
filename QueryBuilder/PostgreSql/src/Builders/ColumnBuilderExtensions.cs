712327f93983ed0c34846ad7202fe3a872fd935b
Y.Paderin <yuriypaderin@gmail.com>
2023-06-05 20:19:34 +0300

    QueryBuilder.PostgreSql: Fix build errors.

--- QueryBuilder/PostgreSql/src/Builders/ColumnBuilderExtensions.cs
+++ QueryBuilder/PostgreSql/src/Builders/ColumnBuilderExtensions.cs
@@ -5,31 +5,33 @@ namespace YuraSoft.QueryBuilder.PostgreSql
 {
     public static class ColumnBuilderExtensions
     {
+		private static readonly ExpressionFactory _factory = ExpressionFactory.Instance;
+
 		public static ColumnBuilder Coalesce(this ColumnBuilder builder, string column, bool value, string? alias = null) =>
-			builder.Column(builder.Factory.Coalesce(column, value), alias);
+			builder.Column(_factory.Coalesce(column, value), alias);
 
 		public static ColumnBuilder Coalesce(this ColumnBuilder builder, string column, string table, bool value, string? alias = null) =>
-			builder.Column(builder.Factory.Coalesce(column, table, value), alias);
+			builder.Column(_factory.Coalesce(column, table, value), alias);
 
 		public static ColumnBuilder Coalesce(this ColumnBuilder builder, string column, ISource source, bool value, string? alias = null) =>
-			builder.Column(builder.Factory.Coalesce(column, source, value), alias);
+			builder.Column(_factory.Coalesce(column, source, value), alias);
 
 		public static ColumnBuilder Coalesce(this ColumnBuilder builder, Func<ExpressionFactory, IExpression> expressionFunction, bool value, string? alias = null) =>
-			builder.Column(builder.Factory.Coalesce(expressionFunction, value), alias);
+			builder.Column(_factory.Coalesce(expressionFunction, value), alias);
 
 		public static ColumnBuilder Coalesce(this ColumnBuilder builder, IExpression expression, bool value, string? alias = null) =>
-			builder.Column(builder.Factory.Coalesce(expression, value), alias);
+			builder.Column(_factory.Coalesce(expression, value), alias);
 
 		public static ColumnBuilder True(this ColumnBuilder builder, string? alias = null) =>
-			builder.Column(builder.Factory.True(), alias);
+			builder.Column(_factory.True(), alias);
 
 		public static ColumnBuilder False(this ColumnBuilder builder, string? alias = null) =>
-			builder.Column(builder.Factory.False(), alias);
+			builder.Column(_factory.False(), alias);
 
 		public static ColumnBuilder Bool(this ColumnBuilder builder, bool value, string? alias = null) => 
-			builder.Column(builder.Factory.Bool(value), alias);
+			builder.Column(_factory.Bool(value), alias);
 
 		public static ColumnBuilder Bool(this ColumnBuilder builder, bool? value, string? alias = null) =>
-			builder.Column(builder.Factory.Bool(value), alias);
+			builder.Column(_factory.Bool(value), alias);
 	}
 }
